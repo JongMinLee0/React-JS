160384e49a364505a224fb4f8388bd7f26561440
JongMinLee <1393062@naver.com>
2019-04-21 23:42:27 +0900

    add : - Movie page 중간 완성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,50 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
+
+
 class App extends Component {
+
+  // Render : componentWillMount() -> render() -> componentDidMount()
+
+  // Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillupdate() -> render() -> componetDidUpdate()
+ 
+  state = {}
+
+  componentDidMount(){
+   this._getMovies();
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi =  () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie 
+      title={movie.title_english} 
+      poster={movie.medium_cover_image}
+      key={movie.id}
+      genres={movie.genres}
+      synopsis={movie.synopsis} />
+    })
+    return movies
+  }
   render() {
     return (
-      <div className="App">
-        <Movie />
-      </div>
+        <div className="App">
+         {this.state.movies ? this._renderMovies() : 'Loading'}
+        </div>
     );
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Movie.css';
 
-class Movie extends Component{
-    render(){
-        return(
-            <h1>hello this is a movie</h1>
-        )
-    }
+
+
+function Movie({title, poster, genres, synopsis}){
+    return (
+        <div className="Movie">
+            <div className="Movie__Columns">
+            <MoviePoster poster = {poster} alt={title}/>
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie_Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movie_Synopsis">
+                    {synopsis}
+                </p>
+            </div>
+        </div>
+    )
 }
 
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genres">{genre}</span>
+    )
+}
+
+function MoviePoster({poster, alt}){
+    return (
+        <img src={poster} alt = {alt} title={alt} className="Movie__Poster" />
+    )
+}
 export default Movie
